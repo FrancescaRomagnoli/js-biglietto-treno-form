d0de9dae2b01fd76a31f1adbc1f6ff1bfb9e4018
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-23 15:56:44 +0200

    add html and variables

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("JS OK");
+// # data
+
+const pricePerKm = 0.21;
+const userDistanceInput = document.getElementById("distance");
+const userAgeRangeInput = document.getElementById("age-range");
+
+const submitButton = document.getElementById("generate-btn");
+const cancelButton = document.getElementById("cancel-btn");
+
+const ticketForm = document.getElementById("ticket-info-form");
+
+// # discounts
+const minorDiscount = 20;
+const seniorDiscountt = 40;
