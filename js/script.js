5b9109e5be0d8f8d97ce596e490417dfc573094d
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-23 16:39:03 +0200

    add price calc

--- js/script.js
+++ js/script.js
@@ -4,11 +4,49 @@ const pricePerKm = 0.21;
 const userDistanceInput = document.getElementById("distance");
 const userAgeRangeInput = document.getElementById("age-range");
 
-const submitButton = document.getElementById("generate-btn");
+const submitButton = document.getElementById("submit-btn");
 const cancelButton = document.getElementById("cancel-btn");
 
 const ticketForm = document.getElementById("ticket-info-form");
 
+// console.log(userDistanceInput.value);
+// console.log(userAgeRangeInput.value);
+// console.log(submitButton.value);
+// console.log(cancelButton.value);
+// console.log(ticketForm.value);
+
 // # discounts
 const minorDiscount = 20;
-const seniorDiscountt = 40;
+const seniorDiscount = 40;
+
+// # calc
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  // # user input data
+  let userDistance = parseInt(userDistanceInput.value);
+  let userAgeRange = userAgeRangeInput.value;
+  let discountPrice = 0;
+  let ticketPrice = userDistance * pricePerKm;
+
+  if (userAgeRangeInput.value === "minor") {
+    discountPrice = (ticketPrice * minorDiscount) / 100;
+  } else if (userAgeRangeInput.value === "senior") {
+    discountPrice = (ticketPrice * seniorDiscount) / 100;
+  }
+
+  console.log(
+    "userDistance :",
+    typeof userDistance,
+    userDistance,
+    "userAgeRange :",
+    typeof userAgeRange,
+    userAgeRange
+  );
+
+  // # calc
+
+  let finalPrice = ticketPrice - discountPrice;
+  console.log(ticketPrice, finalPrice);
+});
